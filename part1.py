f230e79cc9551a7fa1284dcf7e05bcb1e6aa05c9
Jisu Yee <103280924+rosycoloredglasses@users.noreply.github.com>
2022-05-21 03:22:07 +0000

    For part 1, I wrote a code with three variables. I considered all of the possible cases where each number would be the smallest (including the <= sign) in three different code blocks, and wrote what response should be printed for each one.

--- part1.py
+++ part1.py
@@ -15,13 +15,13 @@ Enter another number: 5
 The smallest number is 2
 '''
 
-number = int(input("Enter a number: "))
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter another number: "))
+number3 = int(input("Enter another number: "))
 
-smallest = number
-
-number = int(input("Enter another number: "))
-
-if number < smallest:
-  smallest = number
-  
-print("The smallest number is ", smallest)
+if number1 <= number2 <= number3 or number1 <= number3 <= number2:
+  print("The smallest number is ", number1)
+elif number2 <= number1 <= number3 or number2 <= number3 <= number1:
+  print("The smallest number is ", number2)
+else:
+  print("The smallest number is ", number3)
